15c7a045b1450c7376cfcf1bfb20996e3f1b06b0
Joasia Klasik <asklas@op.pl>
2023-03-18 09:56:03 +0100

    diagrams

--- tests/tests/nowa.cpp
+++ tests/tests/nowa.cpp
@@ -6,9 +6,9 @@ TEST_CASE("ktora klasa jest testowana", "[nowa]")
     nowa n;
     SECTION("co z tej klasy bedzie testowane")
     {
-        REQUIRE(n.getone() == 0);
         REQUIRE(n.getone() == 1);
+        REQUIRE(n.getone() == 0);
     }
 }
 
-// podcza wywoływania dołączaj plik catch_amalgamated.hpp
+// tak kompiluj g++ nowa.cpp ../catch_amalgamated.cpp
